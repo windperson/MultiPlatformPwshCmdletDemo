34902f4217de8ca677a10ae0b1618c021c63ca6c
Isak Pao <windperson@gmail.com>
2024-12-04 00:43:40 +0800

    Windows Workable version

---
+++ Grpc/GreetingGrpcService/Services/GreeterService.cs
@@ -0,0 +1,24 @@
+using Grpc.Core;
+using GreetingGrpcService;
+
+namespace GreetingGrpcService.Services;
+
+public class GreeterService : Greeter.GreeterBase
+{
+    private readonly ILogger<GreeterService> _logger;
+
+    public GreeterService(ILogger<GreeterService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        var postFix = string.IsNullOrEmpty(request.Name) ? "World" : $"{request.Name}";
+        _logger.LogInformation("Returning Hello message to \"{postFix}\"", postFix);
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + postFix
+        });
+    }
+}
